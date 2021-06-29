02fa0eefd9be7741c3aa7756f35f254c4c80b9c7
Bridgett Wheeler <bridgettwheeler@gmail.com>
2021-06-29 11:41:13 -0400

    needed help

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+// question: would nesting my if statements be good here?
+// question: when to use else if and else Vs. if?
+// question: when to use = vs == vs ===?
+function scuberGreetingForFeet(feet){
+  let greeting;
+  if (feet < 401) {
+    greeting = "This one is on me!"
+  }
+  if (feet > 2000) {
+    greeting = "I will gladly take your thirty bucks."
+  }
+  if (feet > 2500) {
+    greeting = "No can do."
+  }
+return greeting;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let greeting;
+  if (city == "NYC") {
+    greeting = "Ok, sounds good."
+  } else {
+    greeting = "No go."
+  }
+  return greeting;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let tipResponse;
+  if (tip == "generous") {
+    tipResponse = "Thank you so much.";
+  }
+  if (tip == "not as generous") {
+    tipResponse = "Thank you.";
+  } else {
+    tipResponse = "Bye.";
+  }
+  return tipResponse;
 }
